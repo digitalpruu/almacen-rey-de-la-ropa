f655da3cf86964e826d421b0c890b2e3c2a21dca
kevinpicon01 <digitalpruu@gmail.com>
2024-10-02 21:53:51 -0500

    Fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -13,8 +13,10 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "En Almacén rey de la ropa, ofrecemos una amplia variedad de ropa para toda la familia, asegurando que cada miembro encuentre lo que necesita.",
-        description2: "Nuestro compromiso es brindar prendas de calidad que se adapten a cada estilo y ocasión, haciendo que cada compra sea especial."
+        description1: "En Almacén Rey de la Ropa, nos dedicamos a ofrecer una amplia gama de prendas que te permiten expresar tu estilo personal con confianza. Desde ropa casual hasta opciones más formales, nuestro objetivo es brindarte piezas que se adapten a cada momento de tu vida.\n" +
+            "Creemos que la ropa no solo cubre, sino que define quién eres. Por eso, cada artículo en nuestro almacén está seleccionado para ofrecerte la mejor combinación de calidad, moda y comodidad. Queremos que cada prenda te haga sentir auténtico y seguro.\n" +
+            "En Almacén Rey de la Ropa, tu estilo es nuestra prioridad. Descubre nuestras colecciones y encuentra las piezas perfectas que reflejen tu verdadero yo.\n" +
+            "Visítanos y experimenta cómo nuestra ropa puede transformar tu estilo y realzar tu individualidad. ¡Estamos aquí para ayudarte a definir tu estilo real!"
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
@@ -28,7 +30,7 @@ const textos = {
         address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/", // Enlace base de Facebook
+        facebook: "https://www.facebook.com/profile.php?id=61566091847313&mibextid=ZbWKwL", // Enlace base de Facebook
         instagram: "https://www.instagram.com/", // Enlace base de Instagram
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
     },
